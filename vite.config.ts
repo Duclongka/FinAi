41869fa44baa850613bbc286907354f7c2abe61d
Duclongka <duclongka@gmail.com>
2026-02-10 23:00:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -18,7 +17,6 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          // Fix: Removed use of __dirname which is not defined in ESM; using path.resolve('.') instead.
           '@': path.resolve('.'),
         }
       }
